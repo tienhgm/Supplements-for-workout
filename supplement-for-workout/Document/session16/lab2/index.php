364ad8ccdb12d3f95610e3fd8d7546ed2d858ff1
tienhgm <47660593+tienhgm@users.noreply.github.com>
2020-11-18 10:22:31 +0700

    xoa bot

--- supplement-for-workout/Document/session16/lab2/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	//load file Connection.php
-	include "Application/Connection.php";
-	//load MVC MonHoc -> chi can load file MonHocController.php la MVC se tu dong hoat dong
-	include "Controllers/MonHocController.php";
-	// include "Controllers/LopController.php";
-	// include "Controllers/SinhVienController.php";
- ?>
--- supplement-for-workout/Document/session16/lab3/index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	//load file Connection.php
-	include "Application/Connection.php";
-	
-	//load file layout
-	include "Views/Layout.php";
- ?>
